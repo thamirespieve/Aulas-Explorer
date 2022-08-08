d59bcd279cea923460c886c0c97f59a9a409eff4
Thamires Pieve <thamires.pieve@gec.inatel.br>
2022-08-08 20:41:03 -0300

    feat: using knex to manipualte the database. Create new routes

---
+++ Stage08/src/database/knex/migrations/20220807170117_createLinks.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = knex =>
+  knex.schema.createTable('links', table => {
+    table.increments('id')
+    table.text('url').notNullable()
+
+    table
+      .integer('note_id')
+      .references('id')
+      .inTable('notes')
+      .onDelete('CASCADE')
+
+    table.timestamp('created_at').default(knex.fn.now())
+  })
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = knex => knex.schema.dropTable('links')
--- Stage08/src/routes/index.js
+++ Stage08/src/routes/index.js
@@ -3,7 +3,10 @@ const Router = require('express')
 const router = Router()
 
 const userRouter = require('./users.routes')
+const noteRouter = require('./notes.routes')
+const tagRouter = require('./tags.routes')
 
 router.use('/users', userRouter)
-
+router.use('/notes', noteRouter)
+router.use('/tags', tagRouter)
 module.exports = router
