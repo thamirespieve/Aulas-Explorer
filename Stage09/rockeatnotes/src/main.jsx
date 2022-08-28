609571dc9cf988ede735c583313dd9c4ab3e27d4
Thamires Pieve <thamires.pieve@gec.inatel.br>
2022-08-28 11:59:41 -0300

    feat: Profile page

--- Stage09/rockeatnotes/src/main.jsx
+++ Stage09/rockeatnotes/src/main.jsx
@@ -4,6 +4,7 @@ import {Details} from './pages/Details/Details'
 import {Home} from './pages/Home'
 import {SingIn} from './pages/SingIn'
 import {SingUp} from './pages/SingUp'
+import {Profile} from './pages/Profile'
 import {ThemeProvider} from "styled-components"
 
 import theme from "./styles/theme"
@@ -13,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <SingUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
