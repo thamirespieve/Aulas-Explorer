256252827806c64b8ccbb41812bd03d17e9d4c86
Thamires Pieve <thamires.pieve@gec.inatel.br>
2022-08-28 17:31:11 -0300

    feat: create routes

--- Stage09/rockeatnotes/src/main.jsx
+++ Stage09/rockeatnotes/src/main.jsx
@@ -11,11 +11,13 @@ import {ThemeProvider} from "styled-components"
 import theme from "./styles/theme"
 import GlobalStyle from "./styles/global"
 
+import {Routes} from "./routes"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
