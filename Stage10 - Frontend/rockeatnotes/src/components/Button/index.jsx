7e102f79e95c3b9c0d1749d40aa23af092f94e5b
Thamires Pieve <thamires.pieve@gec.inatel.br>
2022-09-20 19:06:39 -0300

    feat: function to create user

--- Stage10 - Frontend/rockeatnotes/src/components/Button/index.jsx
+++ Stage10 - Frontend/rockeatnotes/src/components/Button/index.jsx
@@ -1,7 +1,7 @@
 import { Container } from "./style";
 
 export function Button({title,loading=false,...rest}){
-console.log(title)
+
 return (
 <Container type="button" disabled={loading} {...rest}>
   {loading?'Carregando':title}
--- Stage10 - Frontend/rockeatnotes/src/pages/SingUp/index.jsx
+++ Stage10 - Frontend/rockeatnotes/src/pages/SingUp/index.jsx
@@ -1,12 +1,41 @@
+import { useState } from "react"
+import {Link, useNavigate} from "react-router-dom"
 import {FiUser,FiMail,FiLock} from "react-icons/fi"
 
 import {Container, Form, BackgroundImg} from "./style"
 
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
+import { api } from "../../services/api"
 
-import {Link} from "react-router-dom"
 export function SingUp(){
+
+  const [name,setName]= useState("")
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+
+  const navigate= useNavigate()
+
+  async function handleSingUp(){
+    
+    if(!name || !email || !password){
+      return alert("É necessário preencher todos os campos!")
+    }
+
+    await api.post("/users",{name,email,password})
+    .then(()=>{ 
+      alert("Usuário cadastrado com sucesso")
+      navigate("/")
+    })
+    .catch((error)=>{
+      if(error.response){
+        alert(error.response.data.message)
+      }
+      else{
+        alert("Não foi possível cadastrar este usuário :(")
+      }
+    })
+  }
 
   return(
     <Container>
@@ -18,11 +47,11 @@ export function SingUp(){
 
         <h2>Crie sua conta</h2>
 
-        <Input type= "text" placeholder="Nome" icon={FiUser}/>
-        <Input type="email" placeholder="E-mail" icon={FiMail}/>
-        <Input type="password" placeholder="Senha" icon={FiLock}/>
+        <Input type= "text" placeholder="Nome" icon={FiUser} onChange={(event)=>setName(event.target.value)} />
+        <Input type="email" placeholder="E-mail" icon={FiMail} onChange={(event)=>setEmail(event.target.value)} />
+        <Input type="password" placeholder="Senha" icon={FiLock} onChange={(event)=>setPassword(event.target.value)} />
 
-        <Button title="Cadastrar"/>
+        <Button title="Cadastrar" onClick={handleSingUp} />
 
         <Link to="/">Voltar para o login</Link>
       </Form>
