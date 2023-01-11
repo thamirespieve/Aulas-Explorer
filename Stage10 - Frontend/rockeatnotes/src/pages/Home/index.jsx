bb15ff35233ffb58bd91b3b0b6615557fb88b093
Thamires Pieve <thamires.pieve@gec.inatel.br>
2023-01-11 19:09:57 -0300

    style: select tags

--- Stage10 - Frontend/rockeatnotes/src/pages/Home/index.jsx
+++ Stage10 - Frontend/rockeatnotes/src/pages/Home/index.jsx
@@ -5,9 +5,36 @@ import{ButtonText} from "../../components/ButtonText"
 import { Input } from "../../components/Input"
 import {Section} from "../../components/Section"
 import {Note} from "../../components/Note"
+import { useState } from "react"
+import { useEffect } from "react"
+import { api } from "../../services/api"
 
 export function Home(){
+  const [tags, setTags] = useState([])
+  const [tagSelected, setTagSelected] = useState([])
 
+  function handleTagSelected(tag) {
+
+    const alreadySelected = tagSelected.includes(tag)
+console.log(alreadySelected)
+    if(alreadySelected){
+      setTagSelected(prevState => prevState.filter(prevTag => prevTag !== tag))
+    }else{
+      setTagSelected(prevState=> [...prevState,tag])
+    }
+
+  }
+
+  useEffect(()=>{
+
+    async function fetchTags(){
+      let tagResponse = await api.get('/tags')
+      setTags(tagResponse.data)
+    }
+
+    fetchTags()
+
+  },[])
 
   return(
     <Container>
@@ -17,9 +44,22 @@ export function Home(){
       <Header/>
 
       <Menu>
-        <li><ButtonText title="Todos" isActive/></li>
-        <li><ButtonText title="React"/></li>
-        <li><ButtonText title="Nodejs"/></li>
+        <li>
+          <ButtonText 
+            title="Todos" 
+            isActive = {tagSelected.length === 0}
+            onClick = {() => handleTagSelected("all")}
+          /></li>
+        {tags && tags.map((tag)=>
+         ( <li key={String(tag.id)}>
+            <ButtonText 
+               title={tag.name} 
+               isActive = {tagSelected.includes(tag.name)}
+               onClick = {() => handleTagSelected(tag.name)}
+            />
+            </li>)
+
+        )}
 
       </Menu>
 
