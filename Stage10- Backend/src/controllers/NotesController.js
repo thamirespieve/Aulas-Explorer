ed42d45f12c791e457702644bc44e7df94e96520
Thamires Pieve <thamires.pieve@gec.inatel.br>
2023-01-11 19:59:48 -0300

    refactore: adding filter at querys

--- Stage10- Backend/src/controllers/NotesController.js
+++ Stage10- Backend/src/controllers/NotesController.js
@@ -67,6 +67,7 @@ class NotesController {
         .whereLike('notes.title', `%${title}%`)
         .whereIn('name', filterTags)
         .innerJoin('notes', 'notes.id', 'tags.note_id')
+        .groupBy('notes.id')
         .orderBy('notes.title')
     } else {
       notes = await knex('notes')
@@ -85,7 +86,7 @@ class NotesController {
       }
     })
 
-    return response.json({ notesWithTags })
+    return response.json(notesWithTags)
   }
 }
 
